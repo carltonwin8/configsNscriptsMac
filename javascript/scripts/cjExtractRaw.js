bc571337ef9de9137f508ae909df53c680cb4931
Carlton Joseph <carlton.joseph@gmail.com>
2018-03-17 14:26:00 -0700

    add macos support to cjExtractRaw.js

--- javascript/scripts/cjExtractRaw.js
+++ javascript/scripts/cjExtractRaw.js
@@ -7,6 +7,11 @@ const cwd = process.cwd();
 const rawDir = path.join(cwd, "raw");
 const jpgDir = path.join(cwd);
 
+const bin =
+  process.platform === "darwin"
+    ? "/usr/local/Cellar/dcraw/9.27.0_2/bin/dcraw"
+    : "/usr/bin/dcraw";
+
 if (!fs.existsSync(rawDir)) {
   console.log(`Error! Directory does not exist: ${rawDir}`);
   process.exit(-1);
@@ -17,7 +22,7 @@ fs.readdirSync(rawDir).forEach(file => {
   const jpg = file.split(".")[0] + ".thumb.jpg";
   const jpgFrom = path.join(rawDir, jpg);
   const jpgTo = path.join(jpgDir, jpg);
-  execSync(`/usr/bin/dcraw -e ${rawFile}`);
+  execSync(`${bin} -e ${rawFile}`);
   execSync(`mv ${jpgFrom} ${jpgTo}`);
   console.log(jpg);
 });
